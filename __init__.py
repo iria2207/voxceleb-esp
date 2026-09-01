19e7f387af28fb6979423e55422e11b0f441d1a9
iria2207 <iria22072001@gmail.com>
2026-09-01 13:47:38 +0200

    Update and rename __init__.py to src/VOXCELEB_ESP/__init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-"""Componentes del pipeline VoxCeleb-ESP.
-
-El paquete se mantiene deliberadamente ligero: los módulos opcionales de
-entrenamiento y diarización no se importan al inicializarlo.
-"""
